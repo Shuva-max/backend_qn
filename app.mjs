a1f139d3cbc7e6ee8971ae1ff2c565f94d433253
Shuva Paul <shuva575paul@gmail.com>
2024-07-26 21:28:33 +0530

    deployment updates

--- app.mjs
+++ app.mjs
@@ -1,6 +1,6 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import qnCall from './routes/qnCall.mjs'
 import db from './db.mjs';
 
